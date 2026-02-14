2d618bd5f82a611fbe452f545177ba4d225291e1
Ritik-ai-engg <www.ritikrajputcom72060@gmail.com>
2026-02-14 14:24:11 +0530

    Daily prectise

--- Day4/array.js
+++ Day4/array.js
@@ -1,14 +1,14 @@
-let marks=[97,82,75,64,36];
-console.log(marks);
-console.log(marks.length);  //property
+// let marks=[97,82,75,64,36];
+// console.log(marks);
+// console.log(marks.length);  //property
 
 
-console.log(marks[3]);
-marks[3]=66;    //this thing only approve in array not in string so we use str.replace in string
-console.log(marks);
+// console.log(marks[3]);
+// marks[3]=66;    //this thing only approve in array not in string so we use str.replace in string
+// console.log(marks);
 
-// let heroes =[ "ironman","thor","hulk","shaktiman","spiderman","antman"];
-// console.log(heroes);
+//  let heroes =[ "ironman","thor","hulk","shaktiman","spiderman","antman"];
+//   console.log(heroes);
 
 // //print value one by one
 // // console.log(heroes[0]);
@@ -26,55 +26,61 @@ console.log(marks);
 
 
 //another example 
-let cities = ["delhi","pune","mumbai","hyderabad","gurgaon"];
+// let cities = ["delhi","pune","mumbai","hyderabad","gurgaon"];
 
-for(let city of cities){
-    console.log(city.toUpperCase());
-}
-
-
-//array methods
-let foodItems=["potato","apple","litchi","tomato"];
-
-foodItems.push("chips","burger","paneer");  //push method
-console.log(foodItems);
-
-foodItems.pop();    //pop method 
-console.log(foodItems);
-let deletedItem = foodItems.pop();
-console.log("deleted item=",deletedItem);
-
-console.log(foodItems);
-console.log(foodItems.toString());  ///toString method
+// for(let city of cities){
+//     console.log(city.toUpperCase());
+// }
 
 
-let marvelHeroes=["thor","spiderman","ironman"];
-let dcHeroes=["superman","batman"];
-let indianHeroes=["shaktiman","krish"];
-let heroes = marvelHeroes.concat(dcHeroes,indianHeroes);  //concat method
-console.log(heroes);
+// //array methods
+// let foodItems=["potato","apple","litchi","tomato"];
 
-marvelHeroes.unshift("antman");     //unshift method
-console.log("new marvel heroes",marvelHeroes);
+// foodItems.push("chips","burger","paneer");  //push method
+// console.log(foodItems);
 
-marvelHeroes.shift();  //shift method
-console.log(marvelHeroes);
+// // foodItems.pop();    //pop method 
+// // console.log(foodItems);
+// let deletedItem = foodItems.pop();
+// console.log("deleted item=",deletedItem);
 
-console.log(marvelHeroes.slice(1,3)); //slice methode
+// console.log(foodItems);
+// console.log(foodItems.toString());  ///toString method
 
-//splice method
+
+//  let marvelHeroes=["thor","spiderman","ironman"];
+// // let dcHeroes=["superman","batman"];
+// // let indianHeroes=["shaktiman","krish"];
+// // let heroes = marvelHeroes.concat(dcHeroes,indianHeroes);  //concat method
+// // for(let hero of heroes){
+// //     console.log(hero);
+// // }
+
+// marvelHeroes.unshift("antman");     //unshift method
+// console.log("new marvel heroes",marvelHeroes);
+
+// // marvelHeroes.shift();  //shift method
+// // console.log(marvelHeroes);
+
+//  console.log(marvelHeroes.slice(1,3)); //slice methode
+
+// //splice method
 let learn=[1,2,3,4,5,6,7];
 
-//learn.splice(1,2,111,112);
+//learn.splice(1,3,111,112);
 
 //add element
 //learn.splice(3,0,111);
 
+
 //delete element
 //learn.splice(3,1);
 
+
+
 //replace element
 learn.splice(3,1,111);
+console.log(learn);
 
 
 
--- Day4/prectise1.js
+++ Day4/prectise1.js
@@ -15,7 +15,7 @@ console.log(`avg marks of the class = ${avg}`);
 //of 10% OFF on them. change the array to store final price after applying offer 
 let items =[250,645,300,900,50];
 
-// let i=0;
+let i=0;
 // for (let val of items){
 //     console.log(`value at index ${i} = ${val}`);
 //     let offer = val/10;
