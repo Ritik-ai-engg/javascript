2fc0b05d9937e05957cf83ea246fb62c5179a141
Ritik-ai-engg <www.ritikrajputcom72060@gmail.com>
2026-02-14 14:21:32 +0530

    Daily prectise

---
+++ Day7/D0m7.js
@@ -0,0 +1,26 @@
+//////getAttribute(atr)/////////////////////
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name = div.getAttribute("name");
+// console.log(name);
+
+// let para=document.querySelector("p");
+// console.log(para.getAttribute("class"));
+
+// //////setAttribute(Atr)////////////////////
+// console.log(para.setAttribute("class","newclass"));
+
+// console.log(div.setAttribute("name","Ronak"))
+
+
+//////Style//////node.style///////////////////
+let div =document.querySelector("div");
+div.style.backgroundColor="green";
+
+div.style.fontSize="26px";
+
+div.innerText="Ritik";
